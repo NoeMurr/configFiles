057d74c1d2dfaab740832e539cd4c6f33d2f2957
Noè Murr <noe.murr@outlook.com>
2020-01-21 14:29:37 +0100

    21/01/2020

--- files/home/%USER%/.config/polybar/launch.sh
+++ files/home/%USER%/.config/polybar/launch.sh
@@ -9,5 +9,18 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top -c ~/.config/polybar/config-top.ini &
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
+#polybar top -c ~/.config/polybar/config-top.ini &
+#polybar bottom -c ~/.config/polybar/config-bottom.ini &
+
+for m in `xrandr --query | grep " connected " | cut -d ' ' -f 1`; do
+	if xrandr --query | grep "$m" | grep 'primary' > /dev/null; then 
+		traypos='right' monitor=$m polybar --reload top -c \
+			~/.config/polybar/config-top.ini &
+	else 
+		monitor=$m polybar --reload top -c \
+			~/.config/polybar/config-top.ini &
+	fi
+
+	monitor=$m polybar --reload bottom -c  ~/.config/polybar/config-bottom.ini &
+done
+
