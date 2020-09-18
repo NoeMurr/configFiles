7b8dd6937176134387809dc717254af07a467d88
Noè Murr <noe.murr@outlook.com>
2020-09-18 02:55:08 +0200

    today

--- files/home/%USER%/.config/i3/scripts/set-background.sh
+++ files/home/%USER%/.config/i3/scripts/set-background.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-if [ -x `command -v feh` ] &&\
-   [ -f `cat ~/.i3_bg` ]; 
-then 
-    feh --bg-fill `cat ~/.i3_bg`
-    echo 'bg changed'
-    exit 0
-fi
+[ ! -x `command -v feh` ] && echo "command feh not found" && exit -1;
 
-[ ! -x `command -v feh` ] && echo "command feh not found"
-[ ! -f `cat ~/.i3_bg` ] && echo "bg image not found"
+if [ $# -eq 0 ]; then
+	
+	[ ! -f `cat ~/.i3_bg` ] && echo "bg image not found" && exit -1
+
+    feh --bg-fill `cat ~/.i3_bg`
+	echo 'bg changed'
+    exit 0
+else
+	[ ! -f "$1" ] && echo "The file $1 does not exists" && exit -1
+
+	if ! (file "$1" | grep -qE "image|bitmap"); then
+		echo "the file is not an image"
+		exit -1
+	else
+		echo "$1" > ~/.i3_bg
+		feh --bg-fill `cat ~/.i3_bg`
+		exit 0
+	fi
+fi
 
